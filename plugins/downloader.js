80db133ace1a1e6d309b4396ddfcb6bf6248c87b
Aswin Sparky <113657908+A-S-W-I-N-S-P-A-R-K-Y@users.noreply.github.com>
2024-01-24 22:40:03 +0530

    Update downloader.js

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -9,9 +9,9 @@ Sparky(
     type: "user",
   },
   async ({sparky , msg, text}) => {
-const res = await axios.get(`https://aswin-sparky-api.cyclic.app/api/downloader/youtube_play_mp3?apikey=E5BlGG2eGicBFWpxEOHq&search=kerosene+audio+edit`)
+const res = await axios.get(`https://api-viper-x.koyeb.app/api/song?name=${text}`)
 let response = await res.data
-const aud = await (await fetch(`${response.data.url}`)).buffer()
+const aud = await (await fetch(`${response.data.downloadUrl}`)).buffer()
 sparky.sendMessage(`_*Downloading ${response.data.title}*_`)
   }
   );
