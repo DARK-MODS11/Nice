34889a56541f525f6d5a5d58badab7058dd410d8
Aswin Sparky <113657908+A-S-W-I-N-S-P-A-R-K-Y@users.noreply.github.com>
2024-01-24 10:06:21 +0530

    Create downloader.js

---
+++ plugins/downloader.js
@@ -0,0 +1,17 @@
+const { Sparky } = require("../index.js");
+const axios = require("axios");
+const fetch = require("node-fetch")
+
+Sparky(
+  {
+    pattern: "song",
+    desc: "To check ping",
+    type: "user",
+  },
+  async ({sparky , msg}) => {
+const res = await axios.get(`https://aswin-sparky-api.cyclic.app/api/downloader/youtube_play_mp3?apikey=E5BlGG2eGicBFWpxEOHq&search=${text}`)
+let response = await res.data
+const aud = await (await fetch(`${response.data.url}`)).buffer()
+sparky.sendMessage(`_*Downloading ${response.data.title}*_`)
+  }
+  );
