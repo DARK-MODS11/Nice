c9d1d0ad09baadffa95211d1282bd9436cfedca4
Aswin Sparky <113657908+A-S-W-I-N-S-P-A-R-K-Y@users.noreply.github.com>
2024-01-26 02:59:21 +0530

    Update downloader.js

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -28,9 +28,9 @@ Sparky(
   async ({sparky , msg, text}) => {
 //if(!msg.isGroup) 
 //return await sparky.sendMessage(msg.chat, { text: "*This is a Group Command*" },{ quoted: msg})
-const res = await axios.get(`https://aswin-sparky-api.up.railway.app/downloader/yt_video?search=${text}`)
+const res = await axios.get(`https://aswin-sparky-api.up.railway.app/downloader/yt_video?search=kerosine`)
 let response = await res.data
-//sparky.sendMessage(msg.chat, { text: `*Downloading ${response.data.title}*` },{ quoted: msg})
+sparky.sendMessage(msg.chat, { text: `*Downloading ${response.data.title}*` },{ quoted: msg})
 const vid = await (await fetch(`${response.data.url}`)).buffer()
     sparky.sendMessage(msg.chat , {video : vid} , { quoted : msg })
   }
