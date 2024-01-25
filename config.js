6d37a0d5a5266a9655bda1deb4d1d8a283db615c
Aswin Sparky <113657908+A-S-W-I-N-S-P-A-R-K-Y@users.noreply.github.com>
2024-01-25 22:59:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-    SESSION_ID: "X-BOT-MD:BodilyEnjoying",
+    SESSION_ID: "X-BOT-MD:LaurelIsolated",
     DATABASE_URL: "postgresql://aswin:kUznbwKXY4YlS9jnTUpS6Q@lava-primate-8185.8nk.gcp-asia-southeast1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full",
     PREFIX: "^",
     MODE: "public",
-    SUDO: "917594898804,917012984396",
+    SUDO: "917012984396",
 }
