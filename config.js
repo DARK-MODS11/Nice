a0e394018d755ba47b19df2ff88e977965ead867
Aswin Sparky <113657908+A-S-W-I-N-S-P-A-R-K-Y@users.noreply.github.com>
2024-01-25 23:02:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-    SESSION_ID: "X-BOT-MD:LaurelIsolated",
+    SESSION_ID: process.env.SESSION_ID || "",
     DATABASE_URL: "postgresql://aswin:kUznbwKXY4YlS9jnTUpS6Q@lava-primate-8185.8nk.gcp-asia-southeast1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full",
     PREFIX: "^",
     MODE: "public",
