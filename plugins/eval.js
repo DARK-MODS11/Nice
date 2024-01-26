7829d7b6550d14bb3f22ba1ccb5f1aedf894a065
ᴠɪᴘᴇʀ-x <88338865+Viper-x0@users.noreply.github.com>
2024-01-26 13:46:04 +0530

    Added eval.js

--- plugins/eval.js
+++ plugins/eval.js
@@ -1,14 +1,37 @@
-const { Sparky } = require("../index.js");
-/*
-  Sparky(
-  {
-    on: "text",
-    desc: "Runs server code",
-    type: "user",
-  },
-  async ({sparky , msg}) => {
-    
-  }
-);
+const {
+    Sparky
+} = require("../index.js");
+const X = require("../config.js");
 
-*/
+Sparky(
+    {
+        on: "text",
+        desc: "Runs server code",
+        type: "user",
+    },
+    async ({
+        sparky, msg
+    }) => {
+        let sudo = X.SUDO.split(",");
+        if (msg.sender.includes(sudo) || msg.key.fromMe) {
+
+            if (msg.text.startsWith('>')) {
+                var evaluate = true;
+                try {
+                    evaluate = await eval(msg.text.replace('>', '').toString());
+                    try {
+                        evaluate = JSON.stringify(evaluate, null, 2);
+                    } catch {}
+                } catch (e) {
+                    evaluate = e.stack.toString();
+                }
+                await sparky.sendMessage(msg.chat, {
+                    text: evaluate
+                }, {
+                    quoted: msg
+                });
+            }
+        }
+
+    }
+);
