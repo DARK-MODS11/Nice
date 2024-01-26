97d2dde69123068dccd78bace0b468adb9880294
Aswin Sparky <113657908+A-S-W-I-N-S-P-A-R-K-Y@users.noreply.github.com>
2024-01-26 13:51:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ module.exports = {
     DATABASE_URL: "postgresql://aswin:kUznbwKXY4YlS9jnTUpS6Q@lava-primate-8185.8nk.gcp-asia-southeast1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full",
     PREFIX: "^",
     MODE: "public",
-    SUDO: "917012984396",
+    SUDO: "917012984396,919656459062",
 }
