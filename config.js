1a59475bf0509e496cdc27cb2c1758a6c373a04f
Subithra-kumar <168286581+Subithra-kumar@users.noreply.github.com>
2024-06-07 08:44:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = {
     SESSION_ID: process.env.SESSION_ID || "",
     DATABASE_URL: "postgresql://aswin:kUznbwKXY4YlS9jnTUpS6Q@lava-primate-8185.8nk.gcp-asia-southeast1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full",
     PREFIX: "^",
+    BOT_INFO: process.env.BOT_INFO || "𝐄𝐳𝐫𝐚𝐡𝐡-𝐗𝐃;𝐙𝐞𝐭𝐚𝐡𝐡;https://i.imgur.com/nAkM1zV.mp4",
     MODE: "public",
     SUDO: "917012984396,919656459062,918714803727,918891871875,919539612761",
 }
