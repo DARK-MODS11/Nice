cd547e58b823f5680c1069dcfb8db1ae2757f940
Badan Ser ꤸ <100457004+Kingbadan321@users.noreply.github.com>
2024-06-23 14:30:55 +0530

    Update find.js

--- plugins/find.js
+++ plugins/find.js
@@ -7,11 +7,14 @@ const { findMusic, qdownload } = require("../functions.js");
     type: "user",
   },
   async ({sparky, msg, text}) => {
+    try{
     if (!msg.replied) return await msg.reply("*reply to a short video or audio*");
     let buff = await qdownload(msg.replied);
     let result = await findMusic(buff);
     let data = `𝘵𝘪𝘵𝘭𝘦: ${result.title}\n𝘢𝘳𝘵𝘪s𝘵𝘴: ${result.artists}\𝘯𝘳𝘦𝘭𝘦𝘢𝘴𝘦: ${result.release_date}\𝘯𝘺𝘰𝘶𝘵𝘶𝘣𝘦: ${result.youtube}\𝘯𝘴𝘱𝘰𝘵𝘪𝘧𝘺: ${result.spotify}`;
-
-    return await sparky.sendMessage(msg.chat , { text: data } , { quoted: msg })
+    await sparky.sendMessage(msg.chat , { text: data } , { quoted: msg })
+  }catch(error) {
+    return msg.reply(error)
+}
   }
 );
